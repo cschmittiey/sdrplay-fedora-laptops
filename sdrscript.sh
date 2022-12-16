d06a0db2401b74ff1e15ddc8c293abd4b89f30bf
Caleb Smith <cschmittiey@users.noreply.github.com>
2022-12-15 23:19:30 -0500

    Update sdrscript.sh

--- sdrscript.sh
+++ sdrscript.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-sudo -i
+
 dnf install htop vim tmux git cmake SoapySDR SoapySDR-devel rtl-sdr sdrpp gcc-c++
 dnf groupinstall "Development Tools"
 wget https://www.sdrplay.com/software/SDRplay_RSP_API-Linux-3.07.1.run
 chmod +x SDRplay_RSP_API-Linux-3.07.1.run
 ./SDRplay_RSP_API-Linux-3.07.1.run
+sudo systemctl enable --now sdrplay
 git clone https://github.com/pothosware/SoapySDRPlay3
 cd SoapySDRPlay3
 mkdir build
