76c01abcb1ce6ab2580e1202417d44bae1e0f323
Caleb Smith <cschmittiey@users.noreply.github.com>
2022-12-15 23:20:14 -0500

    Update sdrscript.sh

--- sdrscript.sh
+++ sdrscript.sh
@@ -6,14 +6,15 @@ dnf groupinstall "Development Tools"
 wget https://www.sdrplay.com/software/SDRplay_RSP_API-Linux-3.07.1.run
 chmod +x SDRplay_RSP_API-Linux-3.07.1.run
 ./SDRplay_RSP_API-Linux-3.07.1.run
-sudo systemctl enable --now sdrplay
+systemctl daemon-reload
+systemctl enable --now sdrplay
 git clone https://github.com/pothosware/SoapySDRPlay3
 cd SoapySDRPlay3
 mkdir build
 cd build
 cmake ..
 make
-sudo make install
+make install
 echo "/usr/local/lib" >> /etc/ld.so.conf.d/local.conf
 echo "/usr/local/lib64" >> /etc/ld.so.conf.d/local.conf
-sudo ldconfig
+ldconfig
