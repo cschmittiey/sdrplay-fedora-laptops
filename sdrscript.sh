cade311201033e2a47912522c3866fed2861ae1c
Caleb Smith <cschmittiey@users.noreply.github.com>
2022-12-15 22:59:08 -0500

    Create sdrscript.sh

---
+++ sdrscript.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo -i
+dnf install htop vim tmux git cmake SoapySDR SoapySDR-devel rtl-sdr sdrpp gcc-c++
+dnf groupinstall "Development Tools"
+wget https://www.sdrplay.com/software/SDRplay_RSP_API-Linux-3.07.1.run
+chmod +x SDRplay_RSP_API-Linux-3.07.1.run
+./SDRplay_RSP_API-Linux-3.07.1.run
+git clone https://github.com/pothosware/SoapySDRPlay3
+cd SoapySDRPlay3
+mkdir build
+cd build
+cmake ..
+make
+sudo make install
+echo "/usr/local/lib" >> /etc/ld.so.conf.d/local.conf
+echo "/usr/local/lib64" >> /etc/ld.so.conf.d/local.conf
+sudo ldconfig
